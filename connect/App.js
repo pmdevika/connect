8a63ff30400b666e292203f585c8482e5bd81634
Devikapm <pmdevika123@gmail.com>
2024-04-15 11:43:08 +0530

    commited

--- connect/App.js
+++ connect/App.js
@@ -18,6 +18,7 @@ import map from './components/Map';
 import pendinglist from './components/PendingList'
 import { GlobalProvider } from './GlobalContext';
 import Active from './components/active';
+import grievence from './components/grievence';
 
 const Stack = createNativeStackNavigator();
 
@@ -28,9 +29,9 @@ export default function App() {
       <Stack.Navigator>
       <Stack.Screen name="getstarted" component={GetStarted} options={{ title: '' }} />
         <Stack.Screen name="home" component={Home} options={{ title: ' ' }} />
-        <Stack.Screen name="list" component={List} options={{ title: '' }} />
+        <Stack.Screen name="list" component={List} options={{ title: 'Customers List' }} />
         <Stack.Screen name="history" component={History} options={{ title: '' }} />
-        <Stack.Screen name="profile" component={profile} options={{ title: '' }} />
+        <Stack.Screen name="profile" component={profile} options={{ title: 'Personal Information' }} />
         <Stack.Screen name="messages" component={Messages} options={{ title: '' }} />
         <Stack.Screen name="bidding" component={bidding} options={{ title: '' }} />
         <Stack.Screen name="modal" component={modal} options={{ title: '' }} />
@@ -38,6 +39,7 @@ export default function App() {
         <Stack.Screen name="msglist" component={msglist} options={{ title: '' }} />
         <Stack.Screen name="map" component={map} options={{ title: '' }} />
         <Stack.Screen name="pendinglist" component={pendinglist} options={{ title: '' }} />
+        <Stack.Screen name="grievence" component={grievence} options={{ title: '' }} />
         <Stack.Screen name="active" component={Active} options={{ title: 'Active Appointments' }} />
       </Stack.Navigator>
     </NavigationContainer>
